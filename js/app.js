50c220d7715ceda22290df973300677ee1e20aa2
Jesse Mrozowski <jesse.mrozowski@gmail.com>
2023-05-03 15:19:00 -0700

    Completing Lab 3... I think.

--- js/app.js
+++ js/app.js
@@ -28,70 +28,67 @@ YesOrNoGame(Dog,yes); */
 
 let userCorrectGuesses = 0;
 
-function guesingGame()
+alert(`Welcome to my site ${userName}! Let's play a guessing game, please answer yes/no OR y/n`);
+let questionOneGuess = prompt('Do I have a dog?').toLowerCase();
+if(questionOneGuess === 'y' || questionOneGuess === 'yes'){
+  alert('Sorry you are wrong, I don\'t have a dog');
+} else if(questionOneGuess === 'n' || questionOneGuess === 'no'){
+  alert('You are correct!');
+  userCorrectGuesses++;
+}
+else
 {
-  alert(`Welcome to my site ${userName}! Let's play a guessing game, please answer yes/no OR y/n`);
-  let questionOneGuess = prompt('Do I have a dog?').toLowerCase();
-  if(questionOneGuess === 'y' || questionOneGuess === 'yes'){
-    alert('Sorry you are wrong, I don\'t have a dog');
-  } else if(questionOneGuess === 'n' || questionOneGuess === 'no'){
-    alert('You are correct!');
-    userCorrectGuesses++;
-  }
-  else
-  {
-    alert('We can\'t play the game because you didn\'t input a yes or a no');
-  }
+  alert('We can\'t play the game because you didn\'t input a yes or a no');
+}
 
-  let questionTwoGuess = prompt('Do I have a cat?').toLowerCase();
-  if(questionTwoGuess === 'y' || questionTwoGuess === 'yes'){
-    alert('Sorry you are wrong, I don\'t have a cat');
-  } else if(questionTwoGuess === 'n' || questionTwoGuess === 'no'){
-    alert('You are correct!');
-    userCorrectGuesses++;
-  }
-  else
-  {
-    alert('We can\'t play the game because you didn\'t input a yes or a no');
-  }
+let questionTwoGuess = prompt('Do I have a cat?').toLowerCase();
+if(questionTwoGuess === 'y' || questionTwoGuess === 'yes'){
+  alert('Sorry you are wrong, I don\'t have a cat');
+} else if(questionTwoGuess === 'n' || questionTwoGuess === 'no'){
+  alert('You are correct!');
+  userCorrectGuesses++;
+}
+else
+{
+  alert('We can\'t play the game because you didn\'t input a yes or a no');
+}
 
-  let questionThreeGuess = prompt('Do I like video games?').toLowerCase();
-  if(questionThreeGuess === 'y' || questionThreeGuess === 'yes'){
-    alert('You are correct! I love video games!');
-    userCorrectGuesses++;
-  } else if(questionThreeGuess === 'n' || questionThreeGuess === 'no'){
-    alert('Sorry you are wrong, I love video games!');
-  }
-  else
-  {
-    alert('We can\'t play the game because you didn\'t input a yes or a no');
-  }
+let questionThreeGuess = prompt('Do I like video games?').toLowerCase();
+if(questionThreeGuess === 'y' || questionThreeGuess === 'yes'){
+  alert('You are correct! I love video games!');
+  userCorrectGuesses++;
+} else if(questionThreeGuess === 'n' || questionThreeGuess === 'no'){
+  alert('Sorry you are wrong, I love video games!');
+}
+else
+{
+  alert('We can\'t play the game because you didn\'t input a yes or a no');
+}
 
-  let questionFourGuess = prompt('Do I have a bake good cupcakes?').toLowerCase();
-  if(questionFourGuess === 'y' || questionFourGuess === 'yes'){
-    alert('You are correct! My cupcakes are great!');
-    userCorrectGuesses++;
-  } else if(questionFourGuess === 'n' || questionFourGuess === 'no'){
-    alert('Sorry you are wrong, now I won\'t make you any cupcakes!');
-  }
-  else
-  {
-    alert('We can\'t play the game because you didn\'t input a yes or a no');
-  }
+let questionFourGuess = prompt('Do I have a bake good cupcakes?').toLowerCase();
+if(questionFourGuess === 'y' || questionFourGuess === 'yes'){
+  alert('You are correct! My cupcakes are great!');
+  userCorrectGuesses++;
+} else if(questionFourGuess === 'n' || questionFourGuess === 'no'){
+  alert('Sorry you are wrong, now I won\'t make you any cupcakes!');
+}
+else
+{
+  alert('We can\'t play the game because you didn\'t input a yes or a no');
+}
 
-  let questionFiveGuess = prompt('Do I have listen to music while I write code?').toLowerCase();
-  if(questionFiveGuess === 'y' || questionFiveGuess === 'yes'){
-    alert('You are correct! \\m/');
-    userCorrectGuesses++;
-  } else if(questionFiveGuess === 'n' || questionFiveGuess === 'no'){
-    alert('Sorry you are wrong; I\'m listening to music as I write this.');
-  }
-  else
-  {
-    alert('We can\'t play the game because you didn\'t input a yes or a no');
-  }
-  alert(`Thank you for playing my game ${userName} I hope you learned more about me`);
-} 
+let questionFiveGuess = prompt('Do I have listen to music while I write code?').toLowerCase();
+if(questionFiveGuess === 'y' || questionFiveGuess === 'yes'){
+  alert('You are correct! \\m/');
+  userCorrectGuesses++;
+} else if(questionFiveGuess === 'n' || questionFiveGuess === 'no'){
+  alert('Sorry you are wrong; I\'m listening to music as I write this.');
+}
+else
+{
+  alert('We can\'t play the game because you didn\'t input a yes or a no');
+}
+alert(`Thank you for playing my game ${userName} I hope you learned more about me`);
 
 
 
